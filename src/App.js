db58352f0a45d78155f249f2983427ee6ea12ae3
DV <DVislearning@gmail.com>
2019-06-12 11:09:39 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -16,11 +18,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          34
         </a>
       </header>
     </div>
-  );
+  )};
 }
 
 export default App;
