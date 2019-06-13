5bd735e73820ec1e6190c955ef10f4209ea92756
DV <DVislearning@gmail.com>
2019-06-13 04:16:00 -0700

    timer counts down backwards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NONAME } from 'dns';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      initialTime: Date.now(),
+      elapsedInterval: 0,
+      timerDisplay: 1500000,
+      activeTimer: null,
+      isStopped: true,
+      isPaused: false,
+      breakLength: 300000,
+      sessionLength: 1500000,
+    };
+
+    this.timer = this.timer.bind(this);
+  };
+
+  timer(){ setInterval(() => {
+    this.setState({
+      elapsedInterval: this.state.elapsedInterval += 1000,
+      timerDisplay: this.state.timerDisplay -= 1000
+    });
+  }, 1000)};
 
   render(){
+
+    // returns number of minutes in time object
+    const minify = (timeObject) => {
+      return ("0" + (Math.floor(timeObject / 60000) % 60)).slice(-2);
+    }
+
+    // returns number of seconds in time object
+    const secify = (timeObject) => {
+      return ("0" + (Math.floor(timeObject / 1000) % 60)).slice(-2);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,14 +47,16 @@ class App extends Component {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        Time Left:
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.reddit.com/r/squaredcircle"
           target="_blank"
           rel="noopener noreferrer"
         >
-          34
+          { minify(this.state.timerDisplay) } : { secify(this.state.timerDisplay) } ---- {/* minify(this.state.elapsedInterval) } : { secify(this.state.elapsedIntervalc) */}
         </a>
+        <button id="mylittlebutton" onClick={this.timer}>Begin</button>
       </header>
     </div>
   )};
