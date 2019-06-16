e17f67a00997e52091e418c188d67f49057d39f7
DV <DVislearning@gmail.com>
2019-06-16 00:10:28 -0700

    build basic functionality of clock

--- src/App.js
+++ src/App.js
@@ -3,36 +3,84 @@ import logo from './logo.svg';
 import './App.css';
 import { NONAME } from 'dns';
 
+function StartStop(props) {
+  if (props.isStopped === true) {
+    return <button className="mylittlebuttons" id="start-stop" onClick={props.startTimer}>Start</button>
+  } else {
+    return <button className="mylittlebuttons" id="start-stop" onClick={props.stopTimer}>Stop</button>
+  };
+};
+
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state={
-      initialTime: Date.now(),
       elapsedInterval: 0,
       timerDisplay: 1500000,
-      activeTimer: null,
+      timerMode: 'session',
       isStopped: true,
-      isPaused: false,
       breakLength: 300000,
-      sessionLength: 1500000,
+      sessionLength: 1500000
     };
 
-    this.timer = this.timer.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.incrementTime = this.incrementTime.bind(this)
+    this.incrementSession = this.incrementSession.bind(this);
+    this.decrementSession = this.decrementSession.bind(this);
+    this.tester = this.tester.bind(this);
   };
 
-  timer(){ setInterval(() => {
+  tester = (string) => { console.log(string) }
+
+  incrementTime = () => {
+    this.timer = setInterval(() => {
+      this.setState({
+        elapsedInterval: this.state.elapsedInterval += 1000,
+        timerDisplay: this.state.timerDisplay -= 1000
+      });
+    }, 1000)
+  };
+
+
+  startTimer() {
+    if(this.state.isStopped){
+      this.setState({
+        isStopped: false
+      });
+      this.incrementTime();
+    }
+  }
+
+  stopTimer() {
     this.setState({
-      elapsedInterval: this.state.elapsedInterval += 1000,
-      timerDisplay: this.state.timerDisplay -= 1000
+      isStopped: true
     });
-  }, 1000)};
+    clearInterval(this.timer);
+  }
+
+  incrementSession() {
+    if(this.state.sessionLength < 3600000) {
+      this.setState({
+        sessionLength: this.state.sessionLength += 60000
+      });
+    }
+  }
+
+  decrementSession() {
+    if(this.state.sessionLength > 6000) {
+      this.setState({
+        sessionLength: this.state.sessionLength -= 60000
+      });
+    }
+  }
 
   render(){
 
     // returns number of minutes in time object
     const minify = (timeObject) => {
-      return ("0" + (Math.floor(timeObject / 60000) % 60)).slice(-2);
+      return timeObject !== 3600000 ? ("0" + (Math.floor(timeObject / 60000) % 60)).slice(-2) : 60;
     }
 
     // returns number of seconds in time object
@@ -47,16 +95,19 @@ class App extends Component {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        Time Left:
         <a
           className="App-link"
           href="http://www.reddit.com/r/squaredcircle"
           target="_blank"
           rel="noopener noreferrer"
         >
-          { minify(this.state.timerDisplay) } : { secify(this.state.timerDisplay) } ---- {/* minify(this.state.elapsedInterval) } : { secify(this.state.elapsedIntervalc) */}
+          Session Length: { minify(this.state.sessionLength) } : { secify(this.state.sessionLength) }<br/>
+          Time Left: <br/>
+          <span id="time-left">{ minify(this.state.timerDisplay) } : { secify(this.state.timerDisplay) }</span>
         </a>
-        <button id="mylittlebutton" onClick={this.timer}>Begin</button>
+        <div id="start_stop"><StartStop isStopped={this.state.isStopped} startTimer={this.startTimer} stopTimer={this.stopTimer}/></div>
+        <button className="mylittlebuttons" onClick={this.incrementSession}>+</button>
+        <button className="mylittlebuttons" onClick={this.decrementSession}>-</button>
       </header>
     </div>
   )};
