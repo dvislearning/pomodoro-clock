66cf451b6c529ecaa18f0781dd09a711e93c6a5e
DV <DVislearning@gmail.com>
2019-08-23 00:34:15 -0700

    Updates

---
+++ precache-manifest.40f93c1550bd9b8872ce1186312efccb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd66971d796377eb1874dab4b557a8f",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "af3064c877386982ff46",
+    "url": "/pomodoro-clock/static/css/main.c86eaadc.chunk.css"
+  },
+  {
+    "revision": "06cdd88a5f5c74f1e274",
+    "url": "/pomodoro-clock/static/js/2.c629263e.chunk.js"
+  },
+  {
+    "revision": "af3064c877386982ff46",
+    "url": "/pomodoro-clock/static/js/main.f72ad470.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "a72013cfad4a51e3e04ea39207ff71b5",
+    "url": "/pomodoro-clock/static/media/beep.a72013cf.mp3"
+  },
+  {
+    "revision": "d5055a1facad2704f72fff3539575166",
+    "url": "/pomodoro-clock/static/media/vapor.d5055a1f.jpg"
+  }
+]);
